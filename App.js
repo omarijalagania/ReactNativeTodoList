6fe5366abe6374f8b3a938c61df71837ac04a2d3
Omari Jalagania <72104525+omarijalagania@users.noreply.github.com>
2021-05-06 14:59:47 +0400

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+
+const [goal, setGoal] = useState('')
+const [listGoals, setListGoals] = useState([]);
+const textHandler = (enteredGoal) => {
+  setGoal(enteredGoal);
+}
+
+const pressHandler = () => {
+  setListGoals(currentGoal => [...listGoals, goal])
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add Goal"
+          style={styles.inputText}
+          onChangeText={textHandler}
+          value={goal}
+          />
+          <Button title="Add" onPress={pressHandler}/>
+      </View>
+      <ScrollView>
+        {listGoals.map((goal, index) => {
+
+          return(
+            <View key={index} style={styles.listStyle}>
+              <Text>
+                {goal}
+              </Text>
+            </View>
+          )
+        })}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+
+  inputText: {
+    borderColor: 'black', 
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    width: '80%',
+  },
+  listStyle: {
+    padding: 10,
+    backgroundColor: '#ccc',
+    borderRightColor: 'black',
+    borderWidth: 1,
+    marginTop: 20,
+  }
 });
